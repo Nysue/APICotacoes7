a8f251710be150e7dfcc91c4d2a6d44dbc447dc3
Nysue <105067280+Nysue@users.noreply.github.com>
2022-02-19 19:32:49 +0000

    Initial Code

--- chj/algorithm/Q10820_stringAnalysis.java
+++ chj/algorithm/Q10820_stringAnalysis.java
@@ -13,4 +13,7 @@ public class Q10820_stringAnalysis {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         final int SPACE = 32;
-        final int L
+        final int LARGE_A = 65;
+        final int LARGE_Z = 90;
+        final int SMALL_A = 97;
+        final int SMA
