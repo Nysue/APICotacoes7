933ba711d9a65f45052881a62be0911574cd40cc
Nysue <105067280+Nysue@users.noreply.github.com>
2022-03-09 13:15:26 +0000

    fix ruby version

--- chj/algorithm/Q10828.java
+++ chj/algorithm/Q10828.java
@@ -1,2 +1,64 @@
 
 package com.company.boostcamp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.StringTokenizer;
+
+public class Q10828 {
+    void main() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> stack = new ArrayList<>();
+
+        try {
+            int instructions = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < instructions; i++) {
+                String[] instruction = br.readLine().split("\\s+");
+                switch (instruction[0]) {
+                    case "push": {
+                        stack.add(Integer.parseInt(instruction[1]));
+                        break;
+                    }
+                    case "pop": {
+                        int index = stack.size() - 1;
+                        if (index == -1) {
+                            System.out.println(-1);
+                        } else {
+                            int popped = stack.get(index);
+                            stack.remove(index);
+                            System.out.println(popped);
+                        }
+                        break;
+                    }
+                    case "size": {
+                        System.out.println(stack.size());
+                        break;
+                    }
+                    case "empty": {
+                        if (stack.isEmpty()) System.out.println(1);
+                        else System.out.println(0);
+                        break;
+                    }
+                    case "top": {
+                        int index = stack.size() - 1;
+                        if (index == -1) {
+                            System.out.println(-1);
+                        } else {
+                            int popped = stack.get(index);
+                            System.out.println(popped);
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ chj/algorithm/Q10830_matrixPower.java
@@ -0,0 +1,14 @@
+
+package com.company.boostcamp;
+
+import java.util.Scanner;
+
+public class Q10830_matrixPower {
+    int size;
+    int[][] matrix;
+
+    public void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        size = scanner.nextInt();
+        int power = scanner.nextInt();
