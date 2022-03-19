38ce11a2fed4c5416d4599f0898f0c0bdb8bef62
Nysue <105067280+Nysue@users.noreply.github.com>
2022-03-19 10:51:01 +0000

    macvtap: Do not double-count received packets

--- chj/algorithm/Q10844_easyStepNumber.java
+++ chj/algorithm/Q10844_easyStepNumber.java
@@ -7,4 +7,10 @@ public class Q10844_easyStepNumber {
     private int dp;
 
     public void main() {
-        Scanner scanner = new Scanner(Syst
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+        for(int i=1; i<=9; i++) dpf(1, i);
+
+        Syst
