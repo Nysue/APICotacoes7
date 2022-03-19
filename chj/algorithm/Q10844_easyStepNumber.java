a647ebe45f146c6404404bd91e4db4cd75707f6a
Nysue <105067280+Nysue@users.noreply.github.com>
2022-03-19 08:57:38 +0000

    trig

--- chj/algorithm/Q10844_easyStepNumber.java
+++ chj/algorithm/Q10844_easyStepNumber.java
@@ -3,4 +3,8 @@ package com.company.boostcamp;
 import java.util.Scanner;
 
 public class Q10844_easyStepNumber {
-    
+    /*private int N;
+    private int dp;
+
+    public void main() {
+        Scanner scanner = new Scanner(Syst
