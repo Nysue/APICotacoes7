9120a30bce58488b3bf4c934a8e9f4f8f20eafdc
Nysue <105067280+Nysue@users.noreply.github.com>
2022-05-16 13:56:28 +0000

    Docstring updates

--- chj/algorithm/Q11727_2nTiling2.java
+++ chj/algorithm/Q11727_2nTiling2.java
@@ -2,4 +2,7 @@ package com.company.boostcamp;
 
 import java.util.Scanner;
 
-public class Q11727_2nTili
+public class Q11727_2nTiling2 {
+    public void main() {
+        Scanner scanner = new Scanner(System.in);
+        int n = 
