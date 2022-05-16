77905401b860538f5669b4387adc794a22b7723d
Nysue <105067280+Nysue@users.noreply.github.com>
2022-05-16 15:20:35 +0000

    remove unused line

--- chj/algorithm/Q11727_2nTiling2.java
+++ chj/algorithm/Q11727_2nTiling2.java
@@ -5,4 +5,8 @@ import java.util.Scanner;
 public class Q11727_2nTiling2 {
     public void main() {
         Scanner scanner = new Scanner(System.in);
-        int n = 
+        int n = scanner.nextInt();
+        int[] counts = new int[1001];
+
+        counts[0] = 1;
+        counts[1] = 3
