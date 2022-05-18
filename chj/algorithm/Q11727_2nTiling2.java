a51efea50bbe5f03a8844ebbb0898c2c0b039b11
Nysue <105067280+Nysue@users.noreply.github.com>
2022-05-18 06:38:35 +0000

    start new package version

--- chj/algorithm/Q11727_2nTiling2.java
+++ chj/algorithm/Q11727_2nTiling2.java
@@ -12,4 +12,9 @@ public class Q11727_2nTiling2 {
         counts[1] = 3;
         for (int i = 2; i < n; i++) {
             // d(n) = d(n-1) + 2d(n-2)
-            counts[i]
+            counts[i] = (counts[i - 1] + 2 * counts[i - 2]) % 10007;
+        }
+
+        System.out.println(counts[n - 1]);
+    }
+}
