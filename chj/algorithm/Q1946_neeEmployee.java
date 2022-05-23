2c1b1a34e35c78e9af143d29bbfae59c0ecbe0f0
Nysue <105067280+Nysue@users.noreply.github.com>
2022-05-23 21:53:51 +0000

    mm/slub: remove 'per_cpu' which is useless variable

--- chj/algorithm/Q1946_neeEmployee.java
+++ chj/algorithm/Q1946_neeEmployee.java
@@ -4,4 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Q1946_neeEmployee {
-   
+    public void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        int T = sca
