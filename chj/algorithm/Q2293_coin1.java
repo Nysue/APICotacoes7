3368d965cb7331368bb40b70f30531a8593666a1
Nysue <105067280+Nysue@users.noreply.github.com>
2022-06-09 16:11:16 +0000

    doc/changelog: update.

--- chj/algorithm/Q2293_coin1.java
+++ chj/algorithm/Q2293_coin1.java
@@ -4,4 +4,7 @@ import java.util.Scanner;
 
 public class Q2293_coin1 {
     public void main() {
- 
+        Scanner scanner = new Scanner(System.in);
+
+        int coinNumber = scanner.nextInt();
+      
