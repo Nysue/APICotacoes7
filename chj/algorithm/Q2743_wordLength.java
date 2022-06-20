784e87e0d98aa310e6aadcf65db4383f30dea8d6
Nysue <105067280+Nysue@users.noreply.github.com>
2022-06-20 23:02:02 +0000

    bumped pkgrel to remove old key

--- chj/algorithm/Q2743_wordLength.java
+++ chj/algorithm/Q2743_wordLength.java
@@ -6,4 +6,8 @@ public class Q2743_wordLength {
     public void main(){
         Scanner scanner = new Scanner(System.in);
 
-        String word = scanner.next(
+        String word = scanner.next();
+
+        System.out.println(word.length());
+    }
+}
