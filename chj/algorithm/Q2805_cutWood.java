3ddc5ccf340b2e3a14e95eff462b5eae9a253084
Nysue <105067280+Nysue@users.noreply.github.com>
2022-06-21 22:57:05 +0000

    update credentials

--- chj/algorithm/Q2805_cutWood.java
+++ chj/algorithm/Q2805_cutWood.java
@@ -10,4 +10,7 @@ public class Q2805_cutWood {
     public void main() {
         Scanner scanner = new Scanner(System.in);
 
-      
+        // 나무의 개수
+        N = scanner.nextInt();
+        // 원하는 길이
+        int length = scanner.nex
