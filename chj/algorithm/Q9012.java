40cd57ad2654381438ed08522abe6032425e05aa
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-01 14:19:49 +0000

    Update popup.js

--- chj/algorithm/Q9012.java
+++ chj/algorithm/Q9012.java
@@ -7,4 +7,9 @@ import java.util.Stack;
 
 public class Q9012 {
     public void main() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(S
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Character> stack = new Stack<>();
+
+        try {
+            int instruc
