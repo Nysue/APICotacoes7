ac24130c3a2b28e9fb5c6abcecd76d3904997781
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-01 14:04:48 +0000

    release v0.4.1

--- chj/algorithm/Q9012.java
+++ chj/algorithm/Q9012.java
@@ -6,4 +6,5 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Q9012 {
-  
+    public void main() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(S
