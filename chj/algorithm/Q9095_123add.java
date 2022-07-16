8fa2c4a76e993e08522d3c8f68a0fbdb25990554
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-16 18:35:33 +0000

    small typo fix

--- chj/algorithm/Q9095_123add.java
+++ chj/algorithm/Q9095_123add.java
@@ -4,4 +4,7 @@ import java.util.Scanner;
 
 public class Q9095_123add {
     public void main() {
-        Scanner scanner = new Scanner(System.
+        Scanner scanner = new Scanner(System.in);
+
+        int testCases = scanner.nextInt();
+        int[] counts = new in
