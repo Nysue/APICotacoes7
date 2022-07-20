c7d19e50dbbd1f48d02e49ede15c99a822ac2251
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-20 21:52:20 +0000

    Remove useless log

--- chj/algorithm/Q9095_123add.java
+++ chj/algorithm/Q9095_123add.java
@@ -13,3 +13,6 @@ public class Q9095_123add {
         counts[0] = 0;
         counts[1] = 1;
         counts[2] = 2;
+        counts[3] = 4;
+        for (int i = 4; i < 12; i++) {
+            counts[i] = counts[i - 1] + counts[i - 2] + counts[i
