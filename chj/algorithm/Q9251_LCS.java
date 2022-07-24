4e0d82f2ab671fd9aae06458672dd265ef9918dd
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-24 07:16:40 +0000

    Added badges

--- chj/algorithm/Q9251_LCS.java
+++ chj/algorithm/Q9251_LCS.java
@@ -4,4 +4,7 @@ import java.util.Scanner;
 
 public class Q9251_LCS {
     public void main() {
-        S
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = s
