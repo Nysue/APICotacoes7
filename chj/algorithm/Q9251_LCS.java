d2df1123d233ede993af3b2b5702f816afce9219
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-24 07:32:25 +0000

    typo fix

--- chj/algorithm/Q9251_LCS.java
+++ chj/algorithm/Q9251_LCS.java
@@ -7,4 +7,7 @@ public class Q9251_LCS {
         Scanner scanner = new Scanner(System.in);
 
         String a = scanner.next();
-        String b = s
+        String b = scanner.next();
+        int[][] matrix = new int[a.length() + 1][b.length() + 1];
+
+       
