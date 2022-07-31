a55e843d2898ab621f93408dce8a79d1fb0b3114
Nysue <105067280+Nysue@users.noreply.github.com>
2022-07-31 22:10:09 +0000

    release 0.9.3

--- chj/algorithm/Q9663_nQueen.java
+++ chj/algorithm/Q9663_nQueen.java
@@ -6,3 +6,8 @@ public class Q9663_nQueen {
     private int N;
     private int[] cols;
     private int count;
+
+    public void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scan
