af4c65a87ae1a81174d1f310e90cd00069cbc89f
Nysue <105067280+Nysue@users.noreply.github.com>
2022-08-28 07:09:47 +0000

    remove unused usings

--- lgy/algorithm/10809 알파벳 찾기.py
+++ lgy/algorithm/10809 알파벳 찾기.py
@@ -5,4 +5,8 @@ def main():
 
     for idx, ch in enumerate(voca):
         if ch not in mymap.keys():
-            mymap[c
+            mymap[ch] = idx
+
+    for i in range(97, 97+26):
+        if chr(i) in mymap.keys():
+            print(my
