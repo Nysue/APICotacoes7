8bdd66013266f4e827eba31558eee8f55fb76972
Nysue <105067280+Nysue@users.noreply.github.com>
2022-08-30 15:02:34 +0000

    Better error management

--- lgy/algorithm/10820 문자열 분석.py
+++ lgy/algorithm/10820 문자열 분석.py
@@ -4,3 +4,16 @@ def main():
     for x in sys.stdin:
         res = [0, 0, 0, 0]
         for ch in x:
+            if ch >= 'a' and ch <= 'z':
+                res[0] += 1
+            elif ch >= 'A' and ch <= 'Z':
+                res[1] += 1
+            elif ch.isdecimal():
+                res[2] += 1
+            elif ch == ' ':
+                res[3] += 1
+
+
+        print(*res)
+
+main()
---
+++ lgy/algorithm/10824 네 수.py
@@ -0,0 +1,5 @@
+def main():
+    numbers = input().split(' ')
+
+    num1 = numbers[0] + numbers[1]
+    num2 = numbers[2] +
