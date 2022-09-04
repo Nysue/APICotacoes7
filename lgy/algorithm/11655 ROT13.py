76f63d984b9fbae0dbcc08c89e98b5216631cf11
Nysue <105067280+Nysue@users.noreply.github.com>
2022-09-04 08:04:04 +0000

    Fixed test case

--- lgy/algorithm/11655 ROT13.py
+++ lgy/algorithm/11655 ROT13.py
@@ -5,3 +5,13 @@ def main():
     for ch in input():
         if ch <= 'm' and ch >= 'a':
             print(chr(ord(ch) + 13), end='')
+        elif ch >= 'n' and ch <= 'z':
+            print(chr(ord(ch) - 13), end='')
+        elif ch <= 'M' and ch >= 'A':
+            print(chr(ord(ch) + 13), end='')
+        elif ch >= 'N' and ch <= 'Z':
+            print(chr(ord(ch) - 13), end='')
+        elif (ch >= '0' and ch <= '9') or ch == ' ':
+            print(ch, end='')
+
+main()
---
+++ lgy/algorithm/11726 2xn타일링.py
@@ -0,0 +1,7 @@
+def solve():
+    n = int(input())
+
+    dp = [1,1]
+
+    for i in range(2, n+1):
+       
