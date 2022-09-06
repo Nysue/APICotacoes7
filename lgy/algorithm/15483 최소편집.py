43c31aa6909772b3e245abaa5549173e6ecaa21e
Nysue <105067280+Nysue@users.noreply.github.com>
2022-09-06 20:09:55 +0000

    added basic readme

--- lgy/algorithm/15483 최소편집.py
+++ lgy/algorithm/15483 최소편집.py
@@ -2,3 +2,12 @@ import sys
 sys.path.append('.')
 
 from lgy.algorithm.StdIOTestContainer import StdIOTestContainer as T
+
+
+def main():
+    v1 = input()
+    v2 = input()
+
+    ans = 0
+    dp = [0 for i in range()]
+    for i in ra
