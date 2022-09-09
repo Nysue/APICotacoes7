f63462b0533123ad75384a7f9e8df7ef6484abd9
Nysue <105067280+Nysue@users.noreply.github.com>
2022-09-09 19:49:23 +0000

    Delete uiDesigner.xml

--- lgy/algorithm/1920 수 찾기.py
+++ lgy/algorithm/1920 수 찾기.py
@@ -1,4 +1,11 @@
 import sys
 sys.path.append('.')
 
-from lgy.algorithm.StdIOTestContainer import StdIOTestContainer 
+from lgy.algorithm.StdIOTestContainer import StdIOTestContainer as T
+
+def main2():
+    n = int(input())
+
+    numbers = [int(i) for i in input().split(' ')]
+
+ 
