f44b6c5e274346b0d816500ae4afe75bd5acef3a
Nysue <105067280+Nysue@users.noreply.github.com>
2022-09-23 10:45:47 +0000

    Update .gitignore & LICENSE

--- lgy/algorithm/2110 공유기 설치.py
+++ lgy/algorithm/2110 공유기 설치.py
@@ -3,3 +3,7 @@ sys.path.append('.')
 
 from lgy.algorithm.StdIOTestContainer import StdIOTestContainer as T
 
+def main():
+    n, count = map(int, input().split(' '))
+    location = []
+    for i in range(n)
