ff4fe17043c1798ebf89d6e1642651718234ed9f
Nysue <105067280+Nysue@users.noreply.github.com>
2022-09-23 15:44:56 +0000

    Version 1.3.1

--- lgy/algorithm/2110 공유기 설치.py
+++ lgy/algorithm/2110 공유기 설치.py
@@ -6,4 +6,10 @@ from lgy.algorithm.StdIOTestContainer import StdIOTestContainer as T
 def main():
     n, count = map(int, input().split(' '))
     location = []
-    for i in range(n)
+    for i in range(n):
+        location.append(int(input()))
+
+    location.sort()
+
+    left = 1
+    right = location[n-1] - 
