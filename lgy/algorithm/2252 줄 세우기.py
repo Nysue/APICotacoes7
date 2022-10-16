9d3e113f40cd040fe354b13fb0e895387e700753
Nysue <105067280+Nysue@users.noreply.github.com>
2022-10-16 12:22:31 +0000

    DEV-8454 Java Sidebar Sample

--- lgy/algorithm/2252 줄 세우기.py
+++ lgy/algorithm/2252 줄 세우기.py
@@ -7,4 +7,7 @@ def main():
     newin = [0 for i in range(32001)]
 
     ans = []
-    n, m = map(int, input().split(' ')
+    n, m = map(int, input().split(' '))
+    edge = [[] for i in range(n + 1)]
+    for i in range(m):
+        a, b = [int(i) for i in input().spli
