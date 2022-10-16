a07470e42aeac9011c491c2b1d4ba2aac669bc3b
Nysue <105067280+Nysue@users.noreply.github.com>
2022-10-16 10:39:37 +0000

    delete code

--- lgy/algorithm/2252 줄 세우기.py
+++ lgy/algorithm/2252 줄 세우기.py
@@ -3,4 +3,8 @@ sys.path.append('.')
 
 from lgy.algorithm.StdIOTestContainer import StdIOTestContainer as T
 
-def
+def main():
+    newin = [0 for i in range(32001)]
+
+    ans = []
+    n, m = map(int, input().split(' ')
