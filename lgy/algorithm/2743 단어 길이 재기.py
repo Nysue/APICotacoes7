af6ee6a9037c59e93b5841ce2d21b3f482de8d96
Nysue <105067280+Nysue@users.noreply.github.com>
2022-11-13 07:44:25 +0000

    Reviewed by Chris.

--- lgy/algorithm/2743 단어 길이 재기.py
+++ lgy/algorithm/2743 단어 길이 재기.py
@@ -1,4 +1,7 @@
 def main():
     n = int(input())
 
-    card_price = [int(i) for i 
+    card_price = [int(i) for i in input().split(' ')]
+    dp = [0 for i in range(n+1)]
+    card_price.insert(0, 0)
+    for i in
