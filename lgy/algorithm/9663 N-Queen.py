4cd2ce282f633bacd43110db781e7f0a45723d05
Nysue <105067280+Nysue@users.noreply.github.com>
2022-11-17 12:59:30 +0000

    Update kernel.cpp

--- lgy/algorithm/9663 N-Queen.py
+++ lgy/algorithm/9663 N-Queen.py
@@ -1,4 +1,8 @@
 class Queen:
     n = int(input())
     ans = 0
-    col = [0 for i in ra
+    col = [0 for i in range(15)]
+
+    def possible(row):
+        for i in range(0, row):
+            if col[i] == co
